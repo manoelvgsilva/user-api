8feacbed83abf830f286e5a7f142a34bf40cb9e1
manoelvgsilva <manoelvgsilva@gmail.com>
2024-08-25 09:19:19 -0300

    refactore route user

--- src/main/java/com/app/user/service/MessageService.java
+++ src/main/java/com/app/user/service/MessageService.java
@@ -2,40 +2,20 @@ package com.app.user.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 /**
  * messageservice.
  */
 @Service
 public class MessageService {
-  private static final Logger logger = LoggerFactory.getLogger(MessageService.class);
-  private final KafkaTemplate<String, String> kafkaTemplate;
-  private static String staticTopicUser;
+  private final Logger logger = LoggerFactory.getLogger(MessageService.class);
 
-  /**
-   * messageservice.
-   *
-   * @param kafkaTemplate the kafkatemplate
-   * @param topicUser the topicuser
-   */
-  @Autowired
-  public MessageService(KafkaTemplate<String, String> kafkaTemplate, @Value(
-      "${topic.user}") String topicUser) {
-    this.kafkaTemplate = kafkaTemplate;
-    staticTopicUser = topicUser;
-  }
-
-  /**
-   * sendmessage.
-   *
-   * @param message the message
-   */
-  public void sendMessage(String message) {
-    logger.info("Message -> {}", message);
-    this.kafkaTemplate.send(staticTopicUser, message);
+  @KafkaListener(topics = "${topic.user}", groupId = "group_id")
+  public void user(String message) throws IOException {
+    logger.info(String.format("#### -> user message -> %d", message));
   }
 }
