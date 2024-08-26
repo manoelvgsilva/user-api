6b16e85f943e83c86b78d3c7d05032f4cf9c4d2c
manoelvgsilva <manoelvgsilva@gmail.com>
2024-08-26 08:37:02 -0300

    correc errors

--- src/main/java/com/app/user/service/MessageService.java
+++ src/main/java/com/app/user/service/MessageService.java
@@ -1,11 +1,10 @@
 package com.app.user.service;
 
+import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
 
 /**
  * messageservice.
