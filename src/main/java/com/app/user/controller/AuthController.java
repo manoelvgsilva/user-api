41c91e222d6192f782942673c96c3cf54e93dddd
manoelvgsilva <manoelvgsilva@gmail.com>
2024-08-27 09:53:23 -0300

    add response

--- src/main/java/com/app/user/controller/AuthController.java
+++ src/main/java/com/app/user/controller/AuthController.java
@@ -4,13 +4,11 @@ import com.app.user.controller.dto.AuthDto;
 import com.app.user.controller.dto.TokenDto;
 import com.app.user.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * authcontroller.
@@ -39,6 +37,7 @@ public class AuthController {
    * @return login
    */
   @PostMapping("/login")
+  @ResponseStatus(HttpStatus.CREATED)
   public TokenDto login(@RequestBody AuthDto authDto) {
     UsernamePasswordAuthenticationToken usernamePassword =
         new UsernamePasswordAuthenticationToken(authDto.username(),
