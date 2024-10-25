a63df928e298fbf68d68fa41f15c9f24c13b62f0
manoelvgsilva <manoelvgsilva@gmail.com>
2024-10-25 13:15:53 -0300

    up attribute

--- src/main/java/com/app/user/repository/UserRedisRepository.java
+++ src/main/java/com/app/user/repository/UserRedisRepository.java
@@ -2,7 +2,6 @@ package com.app.user.repository;
 
 import com.app.user.entity.UserRedis;
 import java.util.Optional;
-import org.bson.types.ObjectId;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +9,7 @@ import org.springframework.stereotype.Repository;
  * userredisrepository.
  */
 @Repository
-public interface UserRedisRepository extends CrudRepository<UserRedis, ObjectId> {
+public interface UserRedisRepository extends CrudRepository<UserRedis, String> {
   Optional<UserRedis> findByEmail(String email);
 
   Optional<UserRedis> findByCpf(String cpf);
